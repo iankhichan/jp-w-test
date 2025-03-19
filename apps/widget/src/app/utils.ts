12ec37204591f097afcbf03b58aa89ff773e88f9
Ian Khi Chan <ian.k@brainrocket.com>
2025-03-19 11:27:10 +0000

    feat: add utility function to get relative time from a timestamp

---
+++ apps/widget/src/app/utils.ts
@@ -0,0 +1,16 @@
+export const getRelativeTime = (timestamp: number) => {
+  const now = Date.now();
+  const diff = now - timestamp;
+
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+
+  if (seconds < 60) return `${seconds} seconds ago`;
+  if (minutes < 60) return `${minutes} minutes ago`;
+  if (hours < 24) return `${hours} hours ago`;
+  if (days < 7) return `${days} days ago`;
+  return `${weeks} weeks ago`;
+};
